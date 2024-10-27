a81c6fc1453e33f7998e1a707d33bc4519450310
Melika Ahmadi <melikaahmadi@Melikas-Air.lan>
2024-10-27 18:27:49 -0400

    Added filtering for rates and genres

---
+++ Code/recommenderapp/filter.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+# from app import app
+from flask import jsonify, request, render_template
+import sys
+import os
+
+app_dir = os.path.dirname(os.path.abspath(__file__))
+code_dir = os.path.dirname(app_dir)
+project_dir = os.path.dirname(code_dir)
+
+
+class Filter:
+
+    df = pd.read_csv(project_dir + "/data/movies.csv")
+    ratings = pd.read_csv(project_dir + "/data/ratings.csv")
+
+    def __init__(self):
+        pass
+
+    def resultsratings(self, rate):
+        res = []
+        movies = []
+        for x in self.df.rows:
+            if rate >= x["rating"]:
+                res.append(x["movieId"])
+        movies = self.df["title"].iloc[self.df["movieId"] in res]
+        return movies
+
+    def resultsgenre(self, genre):
+        res = []
+        for x in self.df.rows:
+            if genre in x["genres"]:
+                res.append(x["title"])
+        return res
+
+    def resultsTop10genre(self, word):
+        return self.resultsgenre(word)[:10]
+
+
+if __name__ == "__main__":
+    app.run()
