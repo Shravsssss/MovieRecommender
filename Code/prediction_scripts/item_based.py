62afda74eabfe64852d07e12ea22104626d859a0
Shravsssss <sravya.bg@gmail.com>
2024-11-01 23:12:25 -0400

    Add licence and copyright

--- Code/prediction_scripts/item_based.py
+++ Code/prediction_scripts/item_based.py
@@ -1,3 +1,14 @@
+# StreamR
+
+# Version: 1.0.0
+# Date Released: 2024-11-01
+# Authors: Sravya Yepuri, Chirag Hegde, Melika Ahmadi Ranjbar
+
+# Licensed under the MIT License.
+# You may obtain a copy of the License at
+
+#     https://opensource.org/licenses/MIT
+
 import pandas as pd
 import warnings
 import os
